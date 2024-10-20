727a293902fe0bf7e8b2529ef8ef04e38ff3fd74
dstan <64676122+dwstan@users.noreply.github.com>
2024-10-19 22:44:28 -0700

    working on remove

--- components/Login.tsx
+++ components/Login.tsx
@@ -28,7 +28,7 @@ const Login = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch("http://192.168.37.101:8080/api/login", {
+      const response = await fetch("localhost:8080/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
