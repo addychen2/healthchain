590a4316c59564d71fea6cc4a62a34f5c354ebfe
sanityl0st <snarns5@gmail.com>
2024-10-20 06:02:12 -0700

    refining meal cards

--- components/ui/MealCard.tsx
+++ components/ui/MealCard.tsx
@@ -10,19 +10,17 @@ interface MealCardProps {
   sodium: number;
 }
 
-const MealCard: React.FC<MealCardProps> = ({ date, time, meal, calories, protein, sugar, sodium }) => {
+const MealCard: React.FC<MealCardProps> = ({ date, time, meal, calories, protein }) => {
   return (
 
     <div className="bg-diet-green-darker rounded-lg p-4 mb-4 shadow-sm">
       <div className="text-white font-semibold mb-2">
-        {date}, {time}, {meal}
+        {meal}, {date}, {time}
 
       </div>
       <div className="text-white text-sm">
         <p>Calories: {calories}</p>
         <p>Protein: {protein}g</p>
-        <p>Sugar: {sugar}g</p>
-        <p>Sodium: {sodium}mg</p>
       </div>
     </div>
   );
